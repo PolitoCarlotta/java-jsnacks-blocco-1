d9a46bedad04539ffa425efe7e9986c60954916c
CarlottaPolito <carlottapolitowd@gmail.com>
2025-07-26 21:42:07 +0200

    snack7

---
+++ src/Snack7.java
@@ -0,0 +1,13 @@
+public class Snack7 {
+    public static void main(String[] args) {
+        //stampa le potenze di 2 fino a mille
+        int numero = 1;
+
+        System.out.println("Le potenze di 2 sono:");
+        while(numero<=1000){
+            System.out.println(numero);
+            numero *=2;
+            
+        }
+}
+}
