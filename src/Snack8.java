d382e1acbb929cac092d9fb593d05635fba4241c
CarlottaPolito <carlottapolitowd@gmail.com>
2025-07-26 22:34:27 +0200

    snack9

---
+++ src/Snack8.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Snack8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //chiedere all'utente un numero di 4 cifre
+        System.out.print("Inserisci un numero di quattro cifre: ");
+        int numero = input.nextInt();
+
+        //calcola la somma delle 4 cifre
+        if(numero>1000 && numero<= 9999){
+            
+            }
+        }
+    }
+
---
+++ src/Snack9.java
@@ -0,0 +1,15 @@
+public class Snack9 {
+public static void main(String[] args) {
+    //calcola la somma dei primi 10 numeri
+
+    int somma = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+
+    System.out.println("La somma dei primi dieci numeri è: " + somma);
+  
+    //calcola la media
+    double media = somma / 10.0;
+
+    System.out.println("La media dei primi dieci numeri è: " + media);
+
+}
+}
