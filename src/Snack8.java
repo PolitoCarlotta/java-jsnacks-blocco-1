cc22442c6506fa77060e5cb5d35ec218ac7c3f61
CarlottaPolito <carlottapolitowd@gmail.com>
2025-07-27 13:08:24 +0200

    snack8

--- src/Snack8.java
+++ src/Snack8.java
@@ -8,10 +8,18 @@ public class Snack8 {
         //chiedere all'utente un numero di 4 cifre
         System.out.print("Inserisci un numero di quattro cifre: ");
         int numero = input.nextInt();
+        int somma = 0;
 
         //calcola la somma delle 4 cifre
         if(numero>1000 && numero<= 9999){
-            
+            while(numero > 0){
+                int cifra = numero % 10;
+                somma += cifra;
+                numero /= 10;
+            }
+            System.out.println("la somma delle cifre inserita è: " + somma);
+            }else {
+                System.out.println("Il numero inserito non è di quattro cifre.");
             }
         }
     }
